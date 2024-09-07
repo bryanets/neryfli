b949810c03e6a80ef668f0a197d5e9602f15ea3c
bryanets <bryantrebejo@hotmail.com>
2024-09-07 00:43:56 -0500

    fix bunget

--- dist/neryfil/server/main.server.mjs
+++ dist/neryfil/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-MWK5OLVE.mjs";import"./chunk-JVPPJ6IG.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-IYB2NM3Y.mjs";import"./chunk-JVPPJ6IG.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
